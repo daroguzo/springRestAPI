4d83cafc196df422f50e5792e1788097c0ae3ed5
daroguzo <kjw2122@naver.com>
2021-05-11 20:29:36 +0900

    201 응답 받기

--- src/test/java/com/daroguzo/springrestapi/events/EventControllerTests.java
+++ src/test/java/com/daroguzo/springrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.daroguzo.springrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +10,48 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class EventControllerTests {
 
+    /*
+    스프링 MVC 테스트 핵심 클래스
+    웹 서버를 띄우지 않고도 스프링 MVC (DispatcherServlet)가 요청을 처리하는 과정을 확인할 수 있기 때문에 컨트롤러 테스트용으로 자주 쓰임.
+     */
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 5, 11, 20, 11))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 5, 12, 18, 20))
+                .beginEventDateTime(LocalDateTime.of(2020, 5, 15, 18, 20))
+                .endEventDateTime(LocalDateTime.of(2020, 5, 16, 18, 20))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("하계역 5번출구")
+                .build();
+
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
-                 .andExpect(status().isCreated());
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
