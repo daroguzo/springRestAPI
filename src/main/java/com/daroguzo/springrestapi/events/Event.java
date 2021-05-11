e3d3d3a3a9ca6cb69de592bcea15a0627c70629f
daroguzo <kjw2122@naver.com>
2021-05-11 20:03:01 +0900

    Event API 테스트 클래스 생성

--- src/main/java/com/daroguzo/springrestapi/events/Event.java
+++ src/main/java/com/daroguzo/springrestapi/events/Event.java
@@ -15,7 +15,7 @@ public class Event {
     private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
-    private String location; // (optional) 이게 없으면 온라인 모임
+    private String location; // (optional) 없으면 온라인 모임
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
