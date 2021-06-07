10b3c3e6e99460f63f1dfaf287c52b72a109dc72
daroguzo <kjw2122@naver.com>
2021-06-08 00:02:11 +0900

    Spring REST Docs Build

--- src/main/java/com/daroguzo/springrestapi/events/EventController.java
+++ src/main/java/com/daroguzo/springrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package com.daroguzo.springrestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -48,6 +49,7 @@ public class EventController {
         EventResource eventResource = new EventResource(event);
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
+        eventResource.add(new Link("/docs/index.html#resource-events-create").withRel("profile"));
         return ResponseEntity.created(createdURI).body(eventResource);
     }
 }
