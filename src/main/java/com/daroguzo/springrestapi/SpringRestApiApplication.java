923020789918047645bfa905b5e44df53e683fe2
daroguzo <kjw2122@naver.com>
2021-06-11 17:12:40 +0900

    set spring security configuration

--- src/main/java/com/daroguzo/springrestapi/SpringRestApiApplication.java
+++ src/main/java/com/daroguzo/springrestapi/SpringRestApiApplication.java
@@ -1,9 +1,7 @@
 package com.daroguzo.springrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
@@ -12,10 +10,4 @@ public class SpringRestApiApplication {
         SpringApplication.run(SpringRestApiApplication.class, args);
 
     }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
