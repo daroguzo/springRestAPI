32e7c7e8e932bda00b10f0f761323d1bac9c88f3
daroguzo <kjw2122@naver.com>
2021-06-21 14:18:32 +0900

    set spring security configuration2

--- src/main/java/com/daroguzo/springrestapi/configs/AppConfig.java
+++ src/main/java/com/daroguzo/springrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package com.daroguzo.springrestapi.configs;
 
+import com.daroguzo.springrestapi.accounts.Account;
+import com.daroguzo.springrestapi.accounts.AccountService;
+import com.daroguzo.springrestapi.accounts.Accountable;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -16,5 +24,24 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account daroguzo = Account.builder()
+                        .email("daroguzo@email.com")
+                        .password("daroguzo")
+                        .roles(Set.of(Accountable.ADMIN, Accountable.USER))
+                        .build();
+                accountService.saveAccount(daroguzo);
+            }
+        };
     }
 }
